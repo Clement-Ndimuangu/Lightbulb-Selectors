86f2b70fca3110fbe2ce2cd2a7b71a695ba9ec59
Clement Ndimuangu <clement.ndimuangu@verizon.com>
2024-09-04 08:28:01 -0500

    added some extra work

--- script.js
+++ script.js
@@ -7,18 +7,31 @@ const subtitle = document.querySelector(".subtitle")
 let clickCount = 0;
 const handleSubtitle = ()=>{
     clickCount++
-    subtitle.innerHTML = `You've clicked the lights ${clickCount} times`
+    if(clickCount == 1){
+        subtitle.innerHTML = `You've clicked the lights ${clickCount} time`  
+    }else{
+        subtitle.innerHTML = `You've clicked the lights ${clickCount} times`
+    }
 }
 
-light1.addEventListener("click",function(){
+light1.addEventListener("mouseenter",function(){
     light1.classList.toggle('active')
     handleSubtitle()
 })
-light2.addEventListener("click",function(){
+light1.addEventListener("mouseleave",function(){
+    light1.classList.toggle('active')
+})
+light2.addEventListener("mouseenter",function(){
     light2.classList.toggle('active')
     handleSubtitle()
 })
-light3.addEventListener("click",function(){
+light2.addEventListener("mouseleave",function(){
+    light2.classList.toggle('active')
+})
+light3.addEventListener("mouseenter",function(){
     light3.classList.toggle('active')
     handleSubtitle()
+})
+light3.addEventListener("mouseleave",function(){
+    light3.classList.toggle('active')
 })
