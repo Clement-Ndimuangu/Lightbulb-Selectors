26a6ee142d003020f4ef4e94fe6ce72b8aeeff08
Clement Ndimuangu <clement.ndimuangu@verizon.com>
2024-09-04 08:05:56 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Write your code here
+
+const light1 = document.querySelector("#lightbulb1")
+const light2 = document.querySelector("#lightbulb2")
+const light3 = document.querySelector("#lightbulb3")
+const subtitle = document.querySelector(".subtitle")
+let clickCount = 0;
+const handleSubtitle = ()=>{
+    clickCount++
+    subtitle.innerHTML = `You've clicked the lights ${clickCount} times`
+}
+
+light1.addEventListener("click",function(){
+    light1.classList.toggle('active')
+    handleSubtitle()
+})
+light2.addEventListener("click",function(){
+    light2.classList.toggle('active')
+    handleSubtitle()
+})
+light3.addEventListener("click",function(){
+    light3.classList.toggle('active')
+    handleSubtitle()
+})
